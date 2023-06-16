b26f869355489435390a1959655e736d54bde222
Федор Абаза <thedross@gmail.com>
2023-06-16 13:16:36 +0300

    Add CustomUser Model, hope migrations are ok

--- api_yamdb/titles/models.py
+++ api_yamdb/titles/models.py
@@ -1,11 +1,12 @@
 from datetime import date
 
 from django.conf import settings
-from django.contrib.auth import get_user_model
 from django.core.validators import MaxValueValidator, RegexValidator
 from django.db import models
 
-User = get_user_model()
+from users.models import CustomUser
+
+User = CustomUser
 
 
 class CustomBaseModel(models.Model):
--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -3,8 +3,11 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
-    moderator = models.BooleanField(default=False)
-    admin = models.BooleanField(default=False)
+    is_moderator = models.BooleanField(default=False)
+    is_admin = models.BooleanField(
+        default=False,
+        help_text='True if user is admin'
+    )
 
     def __str__(self):
         return self.username
