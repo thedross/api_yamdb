fb8203d962d221b6143534e1fb37917978c49505
Федор Абаза <thedross@gmail.com>
2023-06-26 03:14:34 +0300

    Первую часть сделал

--- api_yamdb/users/validators.py
+++ api_yamdb/users/validators.py
@@ -1,6 +1,21 @@
+import re
+
 from django.core.exceptions import ValidationError
 
 
-def validate_username(value):
-    if value.lower() == 'me':
-        raise ValidationError('Значение "me" недопустимо')
+def validate_username(username):
+    """
+    Валидатор для username.
+    Если username НЕ соответствует шаблону, то re.sub вернет True.
+    В случае соответствия шаблону re.sub вернет пустую строку, т.е. False.
+    """
+    if (re.sub(r'^[\w.@+-]+$', '', username)
+       or username.lower() == 'me'):
+        raise ValidationError(
+            f'Ошибка при проверке "{username}" \n'
+            'для имени пользователя можно использовать'
+            ' только буквы, цифры и символы _ . @ + - \n'
+            'Нельзя использовать "me".'
+        )
+    else:
+        return username
